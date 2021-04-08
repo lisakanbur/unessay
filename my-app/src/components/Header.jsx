3aa4e7029394dbe9f689ffc7b212a220d58566f9
lisakanbur <lisakanb@buffalo.edu>
2021-04-08 18:41:50 -0400

    starting

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="App-header">
+                Unessay Site
+            </div>
+        )
+    }
+}
+
+export default Header
---
+++ my-app/src/components/Thesis.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+class Thesis extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                Thesis: As new and innovative technologies emerge, 
+                we are seeing the effects on our society. Technology 
+                companies are at large, and we boast a new generation 
+                of competent technology professionals ready to enter 
+                the workforce. Technology is affecting various industries 
+                such as finance and sciences, and areas such as machine 
+                learning and artificial intelligence have become 
+                increasingly popular. With this popularity, we are 
+                seeing cutting edge advancements in things like 
+                natural language processing and big data, but one 
+                field that is being especially impacted is the fashion 
+                industry. This industry has already experienced a lot 
+                of change, but new technology can have implications on 
+                designers, makers and consumers, while driving a whole
+                new idea of what the entire fashion system should 
+                look like.
+            </div>
+        )
+    }
+}
+
+Thesis.propTypes = {
+
+}
+
+export default Thesis
